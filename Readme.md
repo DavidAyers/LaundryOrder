9f6bbbfaa7c6a68ac48d48d2ec466848ff85b8fa
DESKTOP-C2PQIN0\Dave <DavidAyers1125@gmail.com>
2020-11-05 03:05:55 -0500

    Finished by 11/05/2020

---
+++ Readme.md
@@ -0,0 +1,36 @@
+﻿<h2>Laundry Order - WordPress</h2>
+This website is for friendly laundry service provider offering free pick-up wash & fold laundry services and affordable dry cleaning for homes, businesses and more.
+
+<h2>System Requirements</h2>
+
+<ul>
+
+<li><a  href="https://secure.php.net/">PHP</a> version <strong>5.2.4</strong> or higher.</li>
+
+<li><a  href="https://www.mysql.com/">MySQL</a> version <strong>5.0</strong> or higher.</li>
+
+</ul>
+
+  
+
+<h3>Recommendations</h3>
+
+<ul>
+
+<li><a  href="https://secure.php.net/">PHP</a> version <strong>7.2</strong> or higher.</li>
+
+<li><a  href="https://www.mysql.com/">MySQL</a> version <strong>5.6</strong> or higher.</li>
+
+<li>The <a  href="https://httpd.apache.org/docs/2.2/mod/mod_rewrite.html">mod_rewrite</a> Apache module.</li>
+
+<li><a  href="https://wordpress.org/news/2016/12/moving-toward-ssl/">HTTPS</a> support.</li>
+
+<li>A link to <a  href="https://wordpress.org/">wordpress.org</a> on your site.</li>
+
+</ul>
+
+  
+
+</body>
+
+</html>
---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'n47WXf1ezcd0ge5LuJ06DTGfBjnjgUsg4R6SRF3zDJHtbav1z5EsIsoE3x8gFM0E');
+define('SECURE_AUTH_KEY',  'wd29pALubvKbqt99QD5s2MzE0zU49xxvu86sdVnSsyf9pRvLv9GY5cqwTDNQKMGp');
+define('LOGGED_IN_KEY',    'W6wjnGqhw3jpzR9eY6Muc8N9r0cSQB4qa8LzfneoQU1i0HQcvnV4RXsei7Bf9MD5');
+define('NONCE_KEY',        '5sLb1oVypssFQ3VLXxv5B1CSNmjQQWTVAuRdbqaCQENBrdUXaeT6YT3ofpmuheDq');
+define('AUTH_SALT',        'Do5jL9qwM4THxzCRG1iTjGGQzVHULN6xb6znsiivhd3upboDGu40zua5mwWMoteD');
+define('SECURE_AUTH_SALT', 'xNXPHMqDDRYmNs8cqmaN1eWsYNynV2VQE3Um07xga8hXYFf1TVj7FBs1gnAr9pnw');
+define('LOGGED_IN_SALT',   'DK3MQICshvn9A0PgtxyMMquaXQzTHRYNqvph3JRiDre3IvDeIKGQjcuDMfBiyfPp');
+define('NONCE_SALT',       'bS4R11x4RFV81FiyXhKnGTJ5rvMoi6fY8M2dVoiVJuNBh8Gr2zY0Nb76oJI2x8Xv');
